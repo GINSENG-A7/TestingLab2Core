27155ccd51424b7ab5736a5fd181400fb3d9a285
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2021-09-26 21:50:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TestingLab2Core
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(Math.Cos(5));
+        //    //Console.WriteLine(MyTrigonometry.Cos(5, 400));
+        //    Console.WriteLine(AlternativeTrigonometry.Cos(5));
+        //    Console.WriteLine();
+        //    Console.WriteLine(Math.Log(5));
+        //    Console.WriteLine(AlternativeTrigonometry.Ln(5));
+        //    Console.ReadKey();
+        //}
+    }
+}
